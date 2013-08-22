d9876ddba9b0628025bacd7ba39d26bd17b4cc28
Chris Garry <chrislgarry@gmail.com>
2013-08-22 17:09:41 -0400

    Create srepl.py

---
+++ srepl.py
@@ -0,0 +1,4 @@
+# A short, non-Quine Python script that self-replicates. I fear what will happen if I make it recursive!
+
+import sys, shutil, random, string
+shutil.copy(sys.argv[0],'srepl'+''.join(random.choice(string.ascii_letters + string.digits) for x in range(10))+'.py')
