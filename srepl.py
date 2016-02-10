e485d9fbf7483f874483ac200a7c111c1f7b2e20
Chris Garry <chrislgarry@gmail.com>
2016-02-10 05:02:54 -0500

    Delete srepl.py

--- srepl.py
+++
@@ -1,4 +0,0 @@
-# A short, non-Quine Python script that self-replicates.
-
-import sys, shutil, random, string
-shutil.copy(sys.argv[0],'srepl'+''.join(random.choice(string.ascii_letters + string.digits) for x in range(10))+'.py')
